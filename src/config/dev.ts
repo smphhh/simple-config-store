0688d30e8a035234658bb5ac543952db302bbea6
BigDataSamuli <samuli.holopainen@bigdatasolutions.fi>
2016-08-20 12:48:32 +0300

    Use KMS key in development environment

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -2,16 +2,17 @@
 export function create() {
     let region = 'eu-west-1';
 
+    let masterKeyId = "37c5f34d-fbba-4e3f-af56-e0a89d61f17d";
+
     return {
         region: region,
         server: {
             port: 80
         },
         configStore: {
-            masterKeyId: 'masterKey',
+            masterKeyId: masterKeyId,
             scopeKeyIds: new Map([
-                ['scope1', 'b'],
-                ['scope2', 'd']
+                ['scope1', masterKeyId]
             ])
         },
         dynamoDBDataStore: {
